05ac2ff0f3dacb66eb5e78069a46b60f0f058280
fumiya_kohno <fumiya_kohno@forcia.com>
2016-10-12 21:38:13 +0900

    update

--- hello-io.hs
+++ hello-io.hs
@@ -1,16 +1,13 @@
 #!/usr/bin/env stack
 -- stack runghc
 
-{-# OPTIONS_GHC -fwarn-name-shadowing #-}
 {-# LANGUAGE OverloadedStrings #-}
 import Turtle
 
-echoModified fname = do
-  let file = fromText fname
-  updateDate <- datefile file
-  printf (s%"\t"%utc%"\n") fname updateDate
+nestedIO = do
+  putStr "Hello, "
+  return (putStrLn "I/O!")
 
-  
 main = do
-  args <- arguments
-  mapM echoModified args
+  printIO <- nestedIO
+  printIO
